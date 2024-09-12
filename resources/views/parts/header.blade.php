1175983123ceed3f9fa8bf7863a714d45227c421
Danil Sherbakov <dandiaborn@gmail.com>
2024-09-12 17:07:03 +0300

    header

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,11 +1,12 @@
 <div class="main-bg">
-    <div class="main-bg__wpapper">
-      <h1 class="main-bg__title">Оплата зарубежных сервисов и подписок</h1>
-      <img src="images/placeholder.png" alt="" class="main-bg__img">
-    </div>
+  <div class="main-bg__wpapper">
+    <h1 class="main-bg__title">Оплата зарубежных сервисов и подписок</h1>
+    <img src="images/placeholder.png" alt="" class="main-bg__img">
   </div>
-  <header class="header">
-    <div class="header__wrapper">
+</div>
+<header class="header">
+  <div class="header__wrapper">
+    <div class="header__inner container">
       <div class="header__logo">
         <img src="images/logo.png" alt="" class="header__logo-img">
       </div>
@@ -20,6 +21,39 @@
           <li class="header__item">
             <a href="#" class="header__link">Продукция</a>
           </li>
+        </ul>
+      </nav>
+      <ul class="header__social-links">
+        <li class="header__social-item">
+          <a href="tel:+79582502577" class="header__social-link">+7 (958) 580-25-77</a>
+        </li>
+        <li class="header__social-item">
+          <a href="/" class="header__social-link">WhatsApp</a>
+        </li>
+        <li class="header__social-item">
+          <a href="/" class="header__social-link">Telegram</a>
+        </li>
+      </ul>
+      <div class="header__burger">
+        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
+          <path d="M16 21.333H40" stroke="#1E2532" stroke-width="2" stroke-linecap="round" />
+          <path d="M16 28H40" stroke="#1E2532" stroke-width="2" stroke-linecap="round" />
+          <path d="M16 34.667H40" stroke="#1E2532" stroke-width="2" stroke-linecap="round" />
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="header__mobile header-mobile">
+    <div class="header-mobile__wrapper">
+      <div class="header-mobile__close">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px"
+          width="800px" version="1.1" id="Layer_1" viewBox="0 0 1792 1792" xml:space="preserve">
+          <path
+            d="M1082.2,896.6l410.2-410c51.5-51.5,51.5-134.6,0-186.1s-134.6-51.5-186.1,0l-410.2,410L486,300.4  c-51.5-51.5-134.6-51.5-186.1,0s-51.5,134.6,0,186.1l410.2,410l-410.2,410c-51.5,51.5-51.5,134.6,0,186.1  c51.6,51.5,135,51.5,186.1,0l410.2-410l410.2,410c51.5,51.5,134.6,51.5,186.1,0c51.1-51.5,51.1-134.6-0.5-186.2L1082.2,896.6z" />
+        </svg>
+      </div>
+      <nav class="header__menu">
+        <ul class="header__list">
           <li class="header__item">
             <a href="#" class="header__link">Продукция</a>
           </li>
@@ -31,5 +65,17 @@
           </li>
         </ul>
       </nav>
+      <ul class="header__social-links">
+        <li class="header__social-item">
+          <a href="tel:+79582502577" class="header__social-link">+7 (958) 580-25-77</a>
+        </li>
+        <li class="header__social-item">
+          <a href="/" class="header__social-link">WhatsApp</a>
+        </li>
+        <li class="header__social-item">
+          <a href="/" class="header__social-link">Telegram</a>
+        </li>
+      </ul>
     </div>
-  </header>
+  </div>
+</header>
