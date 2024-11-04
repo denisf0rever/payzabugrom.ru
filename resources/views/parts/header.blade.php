939b380a7f42d9e185b0081e05ab394292b79d56
Danil Sherbakov <dandiaborn@gmail.com>
2024-11-04 16:04:44 +0300

    css fixes

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,7 +1,7 @@
 <div class="main-bg">
   <div class="main-bg__wpapper">
     <h1 class="main-bg__title">Оплата зарубежных сервисов и подписок</h1>
-    <img src="/static/main.svg" alt="" class="main-bg__img">
+    <img src="/static/main.png" alt="" class="main-bg__img">
   </div>
 </div>
 <header class="header">
