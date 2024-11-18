f7bae612eb4c3ba74e41fe7b1f302e320b2d83c6
Danil Sherbakov <dandiaborn@gmail.com>
2024-11-18 16:51:16 +0300

    new page

--- resources/views/service/test.blade.php
+++ resources/views/service/test.blade.php
@@ -6,6 +6,97 @@
 
 @section('content')
 
-
-
+<section class="main__banner banner">
+  <div class="banner__wrapper">
+    <div class="banner__left">
+      <h1 class="banner__title">Оплата зарубежных сервисов и подписок</h1>
+      <h3 class="banner__subtitle">Откройте депозит и получайте доход, даже если нет расчетного счета в банке</h3>
+      <a href="/" class="banner__button">Оформить депозит</a>
+    </div>
+    <div class="banner__right">
+      <img src="/static/main.png" alt="" class="banner__img">
+    </div>
+    <div class="banner__cards banner-cards">
+      <ul class="banner-cards__list">
+        <li class="banner-cards__item">
+          <span class="banner-cards__title">
+            Онлайн
+          </span>
+          <span class="banner-cards__subtitle">
+            Станьте клиентом дистанционно
+          </span>
+          <div class="banner-cards__img-wrapper">
+            <img src="" alt="" class="banner-cards__img">
+          </div>
+        </li>
+        <li class="banner-cards__item">
+          <span class="banner-cards__title">
+            Онлайн
+          </span>
+          <span class="banner-cards__subtitle">
+            Станьте клиентом дистанционно
+          </span>
+          <div class="banner-cards__img-wrapper">
+            <img src="" alt="" class="banner-cards__img">
+          </div>
+        </li>
+        <li class="banner-cards__item">
+          <span class="banner-cards__title">
+            Онлайн
+          </span>
+          <span class="banner-cards__subtitle">
+            Станьте клиентом дистанционно
+          </span>
+          <div class="banner-cards__img-wrapper">
+            <img src="" alt="" class="banner-cards__img">
+          </div>
+        </li>
+        <li class="banner-cards__item">
+          <span class="banner-cards__title">
+            Онлайн
+          </span>
+          <span class="banner-cards__subtitle">
+            Станьте клиентом дистанционно
+          </span>
+          <div class="banner-cards__img-wrapper">
+            <img src="" alt="" class="banner-cards__img">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+<section class="main__info info">
+  <div class="info__wrapper">
+    <h2 class="info__title">Как выиграть призы</h2>
+    <ul class="info__list">
+      <li class="info__item">
+        <span class="info__title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        <span class="info__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatibus
+          dolor corporis nam sequi quasi commodi aspernatur vero? Ipsum ut impedit quas laboriosam ipsam illo maxime
+          atque corrupti suscipit. Excepturi.</span>
+      </li>
+      <li class="info__item">
+        <span class="info__title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        <span class="info__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatibus
+          dolor corporis nam sequi quasi commodi aspernatur vero? Ipsum ut impedit quas laboriosam ipsam illo maxime
+          atque corrupti suscipit. Excepturi.</span>
+        <a href="/" class="info__link">Подробнее о лигах</a>
+      </li>
+      <li class="info__item">
+        <span class="info__title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        <span class="info__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatibus
+          dolor corporis nam sequi quasi commodi aspernatur vero? Ipsum ut impedit quas laboriosam ipsam illo maxime
+          atque corrupti suscipit. Excepturi.</span>
+      </li>
+      <li class="info__item">
+        <span class="info__title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
+        <span class="info__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatibus
+          dolor corporis nam sequi quasi commodi aspernatur vero? Ipsum ut impedit quas laboriosam ipsam illo maxime
+          atque corrupti suscipit. Excepturi.</span>
+        <a href="/" class="info__link">Подробнее о лигах</a>
+      </li>
+    </ul>
+  </div>
+</section>
 @endsection
